9ffb7d7afdff43ce486222fce8025a4ab88baaac
hckwon767 <97076595+hckwon767@users.noreply.github.com>
2024-10-10 22:49:47 +0800

    Update get_proxyip.py

--- get_proxyip.py
+++ get_proxyip.py
@@ -7,7 +7,7 @@ import random
 import zipfile
 import re
 from pathlib import Path
-
+#
 headers={
 	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
 }
